b8ba775c0c4cd7d6ef031787b0c762328079a8c4
prateektut <prateek.tutorshive@gmail.com>
2023-12-12 16:53:49 +0530

    url

--- src/components/urls.js
+++ src/components/urls.js
@@ -1,2 +1,2 @@
-//export const FRONTEND_API= "https://ordermodule-dev.ap-south-1.elasticbeanstalk.com/";
-export const FRONTEND_API= "http://127.0.0.1:5000/"
+export const FRONTEND_API= "https://ordermodule-dev.ap-south-1.elasticbeanstalk.com/";
+//export const FRONTEND_API= "http://127.0.0.1:5000/"
